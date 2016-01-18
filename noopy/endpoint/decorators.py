442cc800c88bb9db736713308888d7f9fb7cfdbc
JoshuaAcurosKim <acuros@kaist.ac.kr>
2016-01-18 18:07:34 +0900

    Register func by endpoint on Endpoint.endpoints

--- noopy/endpoint/decorators.py
+++ noopy/endpoint/decorators.py
@@ -1,17 +1,10 @@
-from functools import wraps
-
 from . import Endpoint
 
 
 def endpoint(path, method):
-    endpoint_ = Endpoint(path, method)
-
     def decorator(func):
-        Endpoint.endpoints[(endpoint_.resource.path, method)] = endpoint_
-
-        @wraps(func)
-        def wrapper(event, context):
-            return func(event, context)
-        return wrapper
+        endpoint_ = Endpoint(path, method)
+        Endpoint.endpoints[endpoint_] = func
+        return func
 
     return decorator
