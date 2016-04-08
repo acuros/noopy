00456fae1d34c8e159aba6b0c5444dd9eba69c79
Acuros <acuros@kaist.ac.kr>
2016-04-08 17:28:42 +0900

    Use noopy.dispatcher

--- noopy/project_template/dispatcher.py
+++ noopy/project_template/dispatcher.py
@@ -1,21 +1,6 @@
-from noopy import lambda_functions
-from noopy.endpoint import Endpoint
-from noopy.endpoint import methods
+import settings
 
+for module in settings.LAMBDA_MODULES:
+    __import__(module)
 
-def dispatch(event, context):
-    print event
-
-    if event['type'] == 'APIGateway':
-        path = event['path']
-        method = getattr(methods, event['method'])
-        endpoint = Endpoint.endpoints[Endpoint(path, method)]
-        return endpoint(event.get('params', {}), context)
-
-    if event['type'] == 'Lambda':
-        funcs = [f for f in lambda_functions if f.func_name == event['function_name']]
-        if len(funcs) != 1:
-            raise ValueError('One and only one function "{}" needed.'.format(event['function_name']))
-        return funcs[0](event.get('params', {}), context)
-
-    raise ValueError('Undefined type: "%s"' % event['type'])
+from noopy.dispatcher import dispatch
